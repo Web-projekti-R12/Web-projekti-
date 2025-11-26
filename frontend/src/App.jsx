af3adbf33f488e98d99264219af069d60a0469bc
Joonas Penttilä <t2pejo06@students.oamk.fi>
2025-11-26 21:33:42 +0200

    Suosikit tehty

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { Routes,Route } from 'react-router-dom'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
 import Navbar from "../components/Navbar";
@@ -10,35 +10,35 @@ import Groups from "../pages/Groups";
 import MovieTestPage from "../pages/Movie_test_page";
 import MovieSearch from "../pages/Search";
 import Registration from "../pages/Registration";
-import ProtectedRoute from '../components/ProtectedRoute';
+import ProtectedRoute from "../components/ProtectedRoute";
+import Favorites from "./Favorites"; // <-- tuodaan suosikit
 
 function App() {
-
   return (
     <>
       <Navbar />
       <Header />
 
-      <main className='main-content'>
+      <main className="main-content">
         <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/login" exact element={<Login />} />
-          <Route path="/search" exact element={<MovieSearch />} />
-          <Route path="/registration" exact element={<Registration />} />
-          <Route path="/movietestpage" exact element={<MovieTestPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/search" element={<MovieSearch />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/movietestpage" element={<MovieTestPage />} />
+          <Route path="/favorites" element={<Favorites />} /> {/* uusi reitti */}
 
-        <Route element={<ProtectedRoute />}>
-          <Route path="/groups" exact element={<Groups />} />
-          
-        </Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/groups" element={<Groups />} />
+          </Route>
 
-          <Route path="/*" exact element={<NotFound />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </main>
 
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
