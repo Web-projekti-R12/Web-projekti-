d5e1609b781f2f264a85fd6a1073a9e74f9fa06e
Eeli-M <eelim04@gmail.com>
2025-12-03 17:52:52 +0200

    Arvostelu melkein valmis

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import reviewController from '../controllers/reviewController.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/', reviewController.getAllReviews);
-router.post('/', reviewController.addReview);
+router.get('/', auth, reviewController.getUserReviews);
+router.post('/', auth, reviewController.addReview);
+router.put('/:rating_id', auth, reviewController.updateReview);
+router.delete('/:rating_id', auth, reviewController.deleteReview);
 
 export default router;
