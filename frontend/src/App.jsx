ede1bcc7921980830b20bdd22a61458d85eaa8d3
Eeli-M <eelim04@gmail.com>
2025-12-08 12:45:14 +0200

    Arvostelujen selaus

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import Registration from "../pages/Registration";
 import Reviews from '../pages/Reviews';
 import ProtectedRoute from "../components/ProtectedRoute";
 import Favorites from "../pages/Favorites";
+import MovieReviews from "../pages/MovieReviews";
 
 function App() {
   const location = useLocation();
@@ -30,12 +31,12 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/search" element={<MovieSearch />} />
           <Route path="/registration" element={<Registration />} />
-          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/reviews/movie/:tmdbId" element={<MovieReviews />} />
 
         <Route element={<ProtectedRoute />}>
           <Route path="/groups" exact element={<Groups />} />
           <Route path="/reviews" exact element={<Reviews />} />
-          
+          <Route path="/favorites" element={<Favorites />} />
         </Route>
 
           <Route path="/*" element={<NotFound />} />
